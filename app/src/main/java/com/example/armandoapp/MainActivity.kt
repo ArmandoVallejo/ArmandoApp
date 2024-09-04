8f4a8b64fb9ec31a23ef2ac853f357a688522239
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-09-04 07:48:42 -0600

    Add: Modifier examples for columns

--- app/src/main/java/com/example/armandoapp/MainActivity.kt
+++ app/src/main/java/com/example/armandoapp/MainActivity.kt
@@ -4,16 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.armandoapp.ui.theme.ArmandoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +32,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+            ){
+                Text(text = "Simple Text")
+                ModifierExample()
+                ModifierExample2()
+                ModifierExample3()
+            }
+
+
             //Layouts
-            Column{
+            /*Column{
                 Text(text="First Row")
                 Text(text="Second Row")
                 Text(text="Third Row")
@@ -36,7 +59,7 @@ class MainActivity : ComponentActivity() {
                     Text(text="label 2")
                 }
                 Greeting(name="World!")
-            }
+            }*/
         }
     }
 }
@@ -55,4 +78,54 @@ fun GreetingPreview() {
     ArmandoAppTheme {
         Greeting("Armando")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample(){
+    Column(
+        modifier = Modifier.padding(24.dp)
+    ){
+        Text(text = "Hello World")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample2(){
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()
+            .clickable(onClick = { clickAction() })
+    ){
+        Text(text = "Hello World")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ModifierExample3(){
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(16.dp)
+            .background(Color.Magenta)
+            .border(width = 2.dp, color = Color.Green)
+            .width(200.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+
+    ) {
+        Text(text = "Item 1")
+        Text(text = "Item 2")
+        Text(text = "Item 3")
+        Text(text = "Item 4")
+        Text(text = "Item 5")
+    }
+
+}
+
+fun clickAction(){
+    println("Column Clicked")
 }
