12505218eed7d8ccf2144044d173f907920ec6cd
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-09-11 07:49:48 -0600

    Add: We work with screens and routes

---
+++ app/src/main/java/com/example/armandoapp/ui/screens/MenuScreen.kt
@@ -0,0 +1,17 @@
+package com.example.armandoapp.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun MenuScreen(navController: NavController){
+    Column {
+        Text(text = "This is the Menu Screen")
+        Button(onClick = {navController.navigate("home")}) {
+        }
+    }
+}
