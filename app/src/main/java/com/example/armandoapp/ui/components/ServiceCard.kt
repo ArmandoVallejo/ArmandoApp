f6b7cdfa12aa10604cf23206f891d15178a2a309
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-11-18 21:30:14 -0600

    Feat :sparkles: (ServiceCard.kt) Added the ServiceCard UI component

--- app/src/main/java/com/example/armandoapp/ui/components/ServiceCard.kt
+++ app/src/main/java/com/example/armandoapp/ui/components/ServiceCard.kt
@@ -1,2 +1,70 @@
 package com.example.armandoapp.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.armandoapp.R
+
+@Preview
+@Composable
+fun ServiceCard(){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.rym),
+                    contentDescription = "Icon of the card",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(end = 8.dp)
+                )
+                Column {
+                    Text(text = "Titulo", style = MaterialTheme.typography.titleLarge)
+
+                    Text(text = "Descripcion", style = MaterialTheme.typography.bodyMedium)
+
+                }
+            }
+
+            Icon(
+                imageVector = Icons.Default.Share,
+                contentDescription = "Icono de compartir",
+                modifier = Modifier.size(20.dp)
+            )
+        }
+    }
+
+}
