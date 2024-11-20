53b5c7f944868e3c53e06cf1a06ed821c34aa0d9
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-11-20 07:53:31 -0600

    Feat :sparkles: Added the ServiceDetailCard component

--- app/src/main/java/com/example/armandoapp/ui/components/ServiceCard.kt
+++ app/src/main/java/com/example/armandoapp/ui/components/ServiceCard.kt
@@ -4,66 +4,92 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.armandoapp.R
 
-@Preview
 @Composable
-fun ServiceCard(){
+fun ServiceCard(
+    id:Int,
+    name: String,
+    username:String,
+    imageURL:String?,
+    onButtonClick:()->Unit
+){
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(3.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Black,
+            contentColor = Color.White
+        )
     ) {
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.rym),
-                    contentDescription = "Icon of the card",
-                    contentScale = ContentScale.FillBounds,
+                AsyncImage(
                     modifier = Modifier
-                        .size(100.dp)
-                        .padding(end = 8.dp)
+                        .size(80.dp)
+                        .padding(10.dp),
+                    model = imageURL,
+                    error = painterResource(R.drawable.rym),
+                    contentDescription = "Service Logo",
+                    contentScale = ContentScale.FillBounds
                 )
-                Column {
-                    Text(text = "Titulo", style = MaterialTheme.typography.titleLarge)
-
-                    Text(text = "Descripcion", style = MaterialTheme.typography.bodyMedium)
+                Column(){
+                    Text(
+                        text = name,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(2.dp, 8.dp, 0.dp, 0.dp)
+                    )
+                    Text(
+                        text = username,
+                        fontSize = 15.sp,
+                        modifier = Modifier
+                            .padding(4.dp, 0.dp)
+                    )
+                }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                IconButton(
+                    modifier = Modifier.padding(20.dp, 0.dp, 0.dp,0.dp),
+                    onClick = { onButtonClick() }){
+                    Icon(
+                        imageVector = Icons.Filled.MoreVert,
+                        contentDescription = "Service details"
+                    )
 
                 }
-            }
 
-            Icon(
-                imageVector = Icons.Default.Share,
-                contentDescription = "Icono de compartir",
-                modifier = Modifier.size(20.dp)
-            )
+            }
         }
     }
 
